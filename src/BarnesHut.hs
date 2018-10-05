f8d2abb78a9640beac8b06edeee8223dd4f9fe4d
namo626 <namo26june@gmail.com>
2018-10-04 22:16:38 -0400

    Abstract the evolution function

--- src/BarnesHut.hs
+++ src/BarnesHut.hs
@@ -1,18 +1,28 @@
 module BarnesHut where
 
-import Types (Vec, mkVector, Particle, mkParticle, position, mass, velocity, (*^), toList, zero,
-              force, getForce)
+import Types
+import Sim (force, getForce)
 import Data.List (partition)
+import Cal
 
 data Tree = Nil
-          | Leaf Particle
-          | Node Info Tree Tree Tree Tree
+          | Leaf !Particle
+          | Node !Info !Tree !Tree !Tree !Tree
           deriving Show
 
-data Info = Info { com :: Vec, totalMass :: Double }
+data Info = Info
+  { centerMass :: !Vec
+  , totalMass  :: !Double
+  , boxSize    :: !Double
+  }
   deriving Show
 
-data Box = Box { xpos :: Double, ypos :: Double, size :: Double }
+getInfo :: Tree -> Info
+getInfo Nil = Info { centerMass = zero, totalMass = 0, boxSize = 0 }
+getInfo (Node info _ _ _ _) = info
+getInfo (Leaf p) = Info { centerMass = position p, totalMass = mass p, boxSize = 0 }
+
+data Box = Box { xpos :: !Double, ypos :: !Double, size :: !Double }
 
 moveBox :: Box -> Double -> Double -> Box
 moveBox box dx dy = box
@@ -21,13 +31,13 @@ moveBox box dx dy = box
   , size = (size box) / 2
   }
 
-combineInfo :: [Tree] -> Info
-combineInfo ts = Info center totalM
+combineInfo :: Double -> [Tree] -> Info
+combineInfo regionSize ts = Info center totalM regionSize
   where
     center       = (1/totalM) *^ (sum $ map combine ts)
     totalM       = sum $ map (totalMass . getInfo) ts
     combine tree = let info = getInfo tree in
-                     (totalMass info) *^ (com info)
+                     (totalMass info) *^ (centerMass info)
 
 
 build :: Box -> [Particle] -> Tree
@@ -45,15 +55,11 @@ build box ps = Node info t1 t2 t3 t4
     (ps3, lp3) = partition (inRegion SW box) lp2
     (ps4, _)   = partition (inRegion SE box) lp3
 
-    info = combineInfo [t1, t2, t3, t4]
+    info = combineInfo regionSize [t1, t2, t3, t4]
     half = (size box) / 2
+    regionSize = (size box) * 2
 
 data Quadrant = NW | NE | SW | SE deriving Eq
-
-getInfo :: Tree -> Info
-getInfo Nil = Info { com = zero, totalMass = 0 }
-getInfo (Node info _ _ _ _) = info
-getInfo (Leaf p) = Info { com = position p, totalMass = mass p }
 
 inRegion :: Quadrant -> Box -> Particle -> Bool
 inRegion qd box p
@@ -71,13 +77,28 @@ singleForce :: Particle -> Tree -> Vec
 singleForce p1 Nil = error "Nil node"
 singleForce p1 (Leaf p2) = getForce p1 p2
 singleForce p1 (Node info t1 t2 t3 t4)
-  | farEnough info = force (m1, r1) (m2, r2)
+  | farEnough p1 info = force (m1, r1) (m2, r2)
   | otherwise = sum $ map (singleForce p1) [t1, t2, t3, t4]
   where
     (m1, r1) = (mass p1, position p1)
-    (m2, r2) = (totalMass info, com info)
+    (m2, r2) = (totalMass info, centerMass info)
 
-farEnough :: Particle -> Info -> Double -> Bool
+farEnough :: Particle -> Info -> Bool
+farEnough p info = (s/d) < theta
+  where
+    s     = boxSize info
+    d     = norm (r1 - r2)
+    r1    = position p
+    r2    = centerMass info
+    theta = 0.1
+
+
+-- | Calculate the net force of particle from a given list of particles
+netForce :: System -> [Vec]
+netForce sys = map (\p -> singleForce p tree) ps
+  where
+    ps = particles sys
+    tree = build (Box 0 0 (universe sys)) ps
 
 
 -- testing
@@ -85,5 +106,14 @@ p1 = mkParticle 0 (mkVector [-10, 10]) zero 10
 p2 = mkParticle 1 (mkVector [5, 12]) zero 10
 p3 = mkParticle 2 (mkVector [12, 5]) zero 6
 box = Box 0 0 20
-
 tree = build box [p1, p2, p3]
+
+sys = mkSystem [ (mkVector [-10, 10], zero, 10)
+               , (mkVector [10, -5], zero, 12)
+               ]
+
+
+evolver :: System -> System
+evolver = evolve 0.1 netForce
+
+states = iterate (evolve 0.1 netForce)
